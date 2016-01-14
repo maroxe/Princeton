84a1992de9adc8d113e1ee7095deffe10a63eb0c
bachir el khadir <maroxe@bachirs-MacBook-Air.local>
2016-01-14 12:58:47 -0500

    homework 5

--- ORF522/problemset5/question6.py
+++ ORF522/problemset5/question6.py
@@ -1,45 +1,42 @@
 import numpy as np
 from scipy.stats import bernoulli
 
-M = 10000 # number of iterations
+M = 100000 # number of iterations
+gamma =  0.1 # Learning rate
 T = 100   # Maturity
-mu = -5   # P(X_i = 1)
-K = 0.     # Strike
-#X = 2*bernoulli.rvs(p, size=(M, T)) - 1
-#X[:,0] = np.random.randint(-20, 20, size=M)
-X = np.random.normal(size=(M, T), scale=300/T, loc=mu*10/T)
+sigma=30   
+mu = -0.5 * sigma
+K = 0.     
+X = np.random.normal(size=(M, T), scale=sigma, loc=mu)
 X[:,0] = np.random.randint(-20, 20, size=M)
 trajectories = np.cumsum(X, axis=1) 
 trajectories = np.vectorize(int)(trajectories)
 
 Smax = np.max(trajectories)
 Smin = np.min(trajectories)
-print(Smax-Smin)
+print(Smax - Smin)
 S = np.arange(Smin, Smax+1)
 t = np.arange(1, T+1)
 S, t = np.meshgrid(S, t)
 
 Q = S * 0.
-#Q[T-1, :] = np.maximum(S[T-1,:] - K, 0)
 Qupdate = Q.copy()
-gamma =  0.2
-print('gamma: ', gamma)
+# Iterative update:
 for traj in trajectories:
     transitions = list(enumerate(traj[:-1]))
     np.random.shuffle(transitions)
     for n, Sn in transitions:
         Snext = traj[n+1]
         Qupdate[n, Sn-Smin] = (1-gamma) * Q[n,Sn-Smin] + \
-                gamma * max(Sn - K,  Q[n+1, Snext-Smin])
+                gamma * max(Snext - K,  Q[n+1, Snext-Smin])
         Q = Qupdate.copy()
+
 flat = [Q.flatten(), S.flatten() - K]
-
 J = np.maximum(*flat)
-
 Control = np.argmax( np.array(flat), axis=0 )
-
 J.shape = S.shape
 Control.shape = S.shape
+
 # Plot
 from matplotlib import pyplot as plt
 import matplotlib.lines as mlines
@@ -50,7 +47,7 @@ for i in range(min(M, 1000)):
     plt.plot(trajectories[i])
 plt.xlabel('t');plt.ylabel('St');
 plt.title('Sample trajectories')
-plt.savefig('traj.png')
+#plt.savefig('traj.png')
 plt.show()
 
 
@@ -60,10 +57,8 @@ plt.colorbar()
 plt.axis([-10, 10, 1, T])
 plt.title('$J(t, S)$')
 plt.xlabel('$S$');plt.ylabel('$t$');
-plt.savefig('option.png')
+#plt.savefig('option.png')
 plt.show()
-
-
 
 # Exec strategy
 labels_str = ('Hold','Exec')
@@ -78,7 +73,7 @@ cbar.ax.set_ylabel('# Strategy', rotation=270)
 plt.axis([-10, 10, 1, T])
 plt.xlabel('$S$');plt.ylabel('$t$');
 plt.title('Exec/Hold')
-plt.savefig('control.png')
+#plt.savefig('control.png')
 plt.show()
 
 
