a1b3b5e7078cf90282ebc268cb846a7ce864b63e
bachir el khadir <maroxe@nat-oitwireless-inside-vapornet100-c-26906.Princeton.EDU>
2016-04-30 23:41:20 -0400

    Convert to relative path

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = open('blog/secretkey.txt').readline()[:-1]
+SECRET_KEY = open(os.path.join(BASE_DIR,'blog/secretkey.txt')).readline()[:-1]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
